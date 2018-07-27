42fc5197eba0fb3f4b5f6f9ee9258104d792acce
Saad 786 <shahid.saad76@gmail.com>
2018-07-27 12:23:28 +0000

    new

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
   # get 'users/:id', to: 'users#show'
   resources :users #, only: [:index,:show,:new]
   root 'users#index'
-  dfugukfdikfdgjdfkjkj
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
