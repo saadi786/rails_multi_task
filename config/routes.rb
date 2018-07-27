d50ce3f15be12492bbdb1f890396adcea7fa5290
Saad 786 <shahid.saad76@gmail.com>
2018-07-27 12:50:31 +0000

    for testing

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,6 @@ Rails.application.routes.draw do
   # get 'users/:id', to: 'users#show'
   resources :users #, only: [:index,:show,:new]
   root 'users#index'
-  dfugukfdikfdgjdfkjkj
-
-
-
-  gcxhjvhkjgfiyhk
+   puts 'This is second branch'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
